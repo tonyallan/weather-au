46b34f512bb11211fba94a6ae49e98309977b1a8
Tony Allan <tony@apms.com.au>
2019-09-07 22:56:18 +1000

    Fixed tests

--- tests/test_observations.py
+++ tests/test_observations.py
@@ -6,29 +6,32 @@ import urllib
 
 from weather import observations
 
-with pytest.raises(KeyError, match='zzz'):
-    obs = observations.Observations('zzz')
+def test_invalid_state():
+    with pytest.raises(KeyError, match='zzz'):
+        obs = observations.Observations('zzz')
 
 obs = observations.Observations('Vic')
-assert obs is not None
-assert obs.identifier == 'IDV60920'
 
-# for station in obs.stations():
-#     print(station['wmo-id'], station['description'])
+def test_obs():
+    assert obs is not None
 
-stations = obs.stations()
-assert len(stations) > 10
+def test_identifier():
+    assert obs.identifier == 'IDV60920'
 
-for station in stations:
-    station_wmo_id = station['wmo-id']
-    assert station_wmo_id is not None
+def test_station_list():
+    stations = obs.stations()
+    assert len(stations) > 10
 
-    station_description = station['description']
-    assert station_description is not None
+    for station in stations:
+        station_wmo_id = station['wmo-id']
+        assert station_wmo_id is not None
 
-    station_air_temperature = obs.air_temperature(station['wmo-id'])
-    #assert station_air_temperature is not None
+        station_description = station['description']
+        assert station_description is not None
+
+        station_air_temperature = obs.air_temperature(station['wmo-id'])
+        #assert station_air_temperature is not None
     
-
-air_temperature_95936 = obs.air_temperature('95936')
-assert air_temperature_95936 is not None
+def test_air_temperature():
+    air_temperature_95936 = obs.air_temperature('95936')
+    assert air_temperature_95936 is not None
--- tests/test_place.py
+++ tests/test_place.py
@@ -8,14 +8,17 @@ from weather import place
 
 obs = place.Place('vic', 'parkville')
 
-air_temperature = obs.air_temperature()
-assert air_temperature is not None
-assert air_temperature > -30.0
-assert air_temperature < 60.0
+def test_air_temperature():
+    air_temperature = obs.air_temperature()
+    assert air_temperature is not None
+    assert air_temperature > -30.0
+    assert air_temperature < 60.0
 
-station_id = obs.station_id()
-assert station_id is not None
-assert len(station_id) == 5
+def test_station_id():
+    station_id = obs.station_id()
+    assert station_id is not None
+    assert len(station_id) == 5
 
-with pytest.raises(urllib.error.HTTPError, match='HTTP Error 404: Not Found'):
-    obs = place.Place('vic', 'zzz')
+def test_404():
+    with pytest.raises(urllib.error.HTTPError, match='HTTP Error 404: Not Found'):
+        obs = place.Place('vic', 'zzz')
